aaf0205110dfecea084321d4d2e0165f5e10781c
Haserand <haserand@gmail.com>
2026-08-02 00:40:57 +0200

    Update Topcompletiondate.test.jsx

--- tests/shared/Topcompletiondate.test.jsx
+++ tests/shared/Topcompletiondate.test.jsx
@@ -6,7 +6,7 @@
 import { describe, it, expect, vi, afterEach } from 'vitest';
 import { render, screen, fireEvent, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
-import TopCompletionDate from '../src/components/shared/TopCompletionDate.jsx';
+import TopCompletionDate from '../../src/components/shared/TopCompletionDate.jsx';
 
 afterEach(() => {
   cleanup();
