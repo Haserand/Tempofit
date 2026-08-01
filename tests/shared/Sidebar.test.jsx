65b945625a91d10739488da4fb89811e8ec2e570
Haserand <haserand@gmail.com>
2026-08-02 00:40:44 +0200

    Update Sidebar.test.jsx

--- tests/shared/Sidebar.test.jsx
+++ tests/shared/Sidebar.test.jsx
@@ -19,7 +19,7 @@
 import { describe, it, expect, vi, afterEach } from 'vitest';
 import { render, screen, fireEvent, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
-import Sidebar from '../src/components/shared/Sidebar.jsx';
+import Sidebar from '../../src/components/shared/Sidebar.jsx';
 
 afterEach(() => {
   cleanup();
