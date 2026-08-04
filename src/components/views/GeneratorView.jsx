b27deb0e2cf6f3b5575799f2940f125bcc27ce3d
Haserand <haserand@gmail.com>
2026-08-04 13:19:46 +0200

    Update GeneratorView.jsx

--- src/components/views/GeneratorView.jsx
+++ src/components/views/GeneratorView.jsx
@@ -32,6 +32,7 @@ export default function GeneratorView({
   setResultsContextLabel, setNoUsableResultsHint, searchTracksByBpm,
   executeGeneration, isGenerating,
   toggleNaughtyMode, changeView,
+  isGuestBarVisible,
 }) {
   const { isNaughtyMode, displaySubtitleGen } = useGeneratorContext();
   const { textColorClass } = theme;
@@ -62,6 +63,7 @@ export default function GeneratorView({
         searchTracksByBpm={searchTracksByBpm}
         executeGeneration={executeGeneration} isGenerating={isGenerating}
         toggleNaughtyMode={toggleNaughtyMode} changeView={changeView}
+        isGuestBarVisible={isGuestBarVisible}
       />
     </div>
   );
